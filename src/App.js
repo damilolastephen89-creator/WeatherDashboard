139954a4c39dab63124518a40e6d37ca1da191d8
damilolastephen89-creator <damilolastephen89@gmail.com>
2026-08-17 23:44:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+async function getWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
 
