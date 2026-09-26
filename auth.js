5bb7941806bdb42d2e33ac3c6187f62c9954223c
Gbenga Olatoyosi Damilola <damilolastephen89@gmail.com>
2026-09-26 17:34:27 +0100

    Remove Google Auth functions from auth.js
    Removed Google authentication initialization and response handling functions.

--- auth.js
+++ auth.js
@@ -6,21 +6,4 @@ function initAuth() {
   });
 }
 
-function initGoogleAuth() {
-  google.accounts.id.initialize({
-    client_id: "YOUR_GOOGLE_CLIENT_ID",
-    callback: handleCredentialResponse
-  });
-  google.accounts.id.renderButton(
-    document.getElementById("googleLogin"),
-    { theme: "outline", size: "large" }
-  );
-}
-
-function handleCredentialResponse(response) {
-  const data = jwt_decode(response.credential);
-  const user = data.email;
-  localStorage.setItem("currentUser", user);
-  document.getElementById("userProfile").innerText = `Welcome, ${user}!`;
-  document.getElementById("loginSection").style.display = "none";
-}
+62023891881-8pcolavqedtnqmpicjf8uch6tooqs1ui.apps.googleusercontent.com
